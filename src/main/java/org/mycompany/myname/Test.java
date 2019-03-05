07f3589066dc2bedb8057cf8d2544c82c514a23e
wagenhuber <wagenhuber@gmx.net>
2019-03-05 17:48:44 +0100

    Initial commit

---
+++ src/main/java/org/mycompany/myname/Test.java
@@ -0,0 +1,14 @@
+package org.mycompany.myname;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getRequestURI());
+    }
+}
